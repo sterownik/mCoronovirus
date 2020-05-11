1f66ababbe414a87d8b1b3e9a1c0bed84a8b37dc
Wojtek <4golda4@gmail.com>
2020-05-12 00:38:20 +0200

    dodanie elementow do htmla oraz stylow css

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const api_url = 'https://api.covid19api.com/dayone/country/poland';
 async function getData() {
     const response = await fetch(api_url);
     const data = await response.json();
-    console.log(data[data.length - 1].Recovered);
+    console.log(data[data.length - 1].Confirmed);
 }
 getData();
