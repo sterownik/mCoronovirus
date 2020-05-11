5e131c4258b16a9074901e4bbe7fbc3ceeab1f0e
Wojtek <4golda4@gmail.com>
2020-05-12 01:08:07 +0200

    dodanie wykresow

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 const api_url = 'https://api.covid19api.com/dayone/country/poland';
+var data1 = "cono";
+var data3;
 
 async function getData() {
     const response = await fetch(api_url);
     const data = await response.json();
     console.log(data[data.length - 1].Confirmed);
+    data3 = data[data.length - 1].Confirmed;
+    data1 = data[data.length - 6].Date;
+    console.log(typeof data3);
+
 }
 getData();
+
+setTimeout(() => {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [data1, 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [data3, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                    'rgba(255, 159, 64, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}, 3000);
